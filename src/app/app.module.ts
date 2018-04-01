55f399553f82b9bb4b705cb296e542cf3009a1dc
visurel <martin@visurel.com>
2018-04-01 19:58:31 +0200

    add example of bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {MdcButtonModule, MdcThemeModule} from '@angular-mdc/web';
 
 
 @NgModule({
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdcButtonModule,
+    MdcThemeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
